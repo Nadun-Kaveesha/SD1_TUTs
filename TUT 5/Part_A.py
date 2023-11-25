1af19db9e1237d70b27badcffc1e9326daca76cb
Nadun-Kaveesha <147963509+Nadun-Kaveesha@users.noreply.github.com>
2023-11-25 15:54:20 +0530

    part c

--- TUT 5/Part_A.py
+++ TUT 5/Part_A.py
@@ -1,6 +1,10 @@
 name=str(input("Please enter your full name :"))
+
 #part a
 print(f'{name} is your full name')
 
 #part B
 print(f'{name[0]} is your first letter of the name')
+
+#part c
+print(f'{name[0:5]} are the first 5 letters of your name')
