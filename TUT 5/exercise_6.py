e7a3a49357cccd52c2e866654aadf72e66df19d0
Nadun-Kaveesha <147963509+Nadun-Kaveesha@users.noreply.github.com>
2023-11-25 18:56:30 +0530

    Merge branch 'main' of https://github.com/Nadun-Kaveesha/SD1_TUTs

---
+++ TUT 5/exercise_6.py
@@ -0,0 +1,12 @@
+secret_word="apple"
+turns=1
+guess=''
+
+
+while True:
+    if turns <= len(secret_word)+3 :
+        guess=str(input("Make your guess :"))
+        turns+=1
+    else:
+        print("Sorry your turns are over")
+        break
