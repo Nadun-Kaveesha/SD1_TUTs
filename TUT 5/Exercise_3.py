809acfccbfe7e15d1afcc29c5fc261264db51180
Nadun-Kaveesha <147963509+Nadun-Kaveesha@users.noreply.github.com>
2023-11-25 16:14:18 +0530

    finished

---
+++ TUT 5/Exercise_3.py
@@ -0,0 +1,7 @@
+name=str(input("Please enter your 6 Character user name :"))
+
+length=len(name)
+if length==6:
+    print("You have successfully enterd your username")
+else:
+    print("Your user name is incorrect")
