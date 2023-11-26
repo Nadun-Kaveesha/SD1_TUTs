64805a45b80a78e3c6eef35a1dd078e22fd66601
Nadun-Kaveesha <147963509+Nadun-Kaveesha@users.noreply.github.com>
2023-11-26 22:39:04 +0530

    guessing game finished

--- TUT 5/exercise_6.py
+++ TUT 5/exercise_6.py
@@ -1,12 +1,28 @@
-secret_word="apple"
-turns=1
-guess=''
+secret_word = "apple"
+turns = 1
+guessed_letters = []
 
+print("Let's play Guessing game")
+print(f'You have {len(secret_word) + 3} turns to guess the word!')
+print("_ " * len(secret_word))
 
 while True:
-    if turns <= len(secret_word)+3 :
-        guess=str(input("Make your guess :"))
-        turns+=1
+    if turns <= len(secret_word) + 3:
+        guess = str(input("\nMake your guess: "))
+
+        for letter in secret_word:
+            if letter in guessed_letters or letter == guess:
+                print(f' {letter} ', end='')
+                if letter not in guessed_letters:
+                    guessed_letters.append(letter)
+            else:
+                print('_ ', end='')
+
+        if all(letter in guessed_letters for letter in secret_word):
+            print("\nCongratulations! You guessed the word!")
+            break
+
+        turns += 1
     else:
-        print("Sorry your turns are over!")
+        print("\nSorry, your turns are over!")
         break
